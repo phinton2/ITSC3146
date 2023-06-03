213fac04c1def9ad59cee49d8060b09446be20a8
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2023-06-03 15:54:29 -0400

    Update array_ptrs.cpp

--- array_ptrs.cpp
+++ array_ptrs.cpp
@@ -1,43 +1,43 @@
+// Paydreanne E. Hinton
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    // Instruction 1
     int myInts[4];
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++) { // add integers to myInts array
         cin >> myInts[i];
     }
     int *my_ptrs[4];
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++) { // assign memory address of myInts to myPtrs to assign values
         my_ptrs[i] = &myInts[i];
     }
     
     cout << "\nCurrent values in myInts: ";
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++) { // displays unordered values of myInts
         cout << myInts[i] << " ";
     }
     cout << "\nCurrent values in my_ptrs: ";
-    for (int i = 0; i < 4;i++) {
+    for (int i = 0; i < 4;i++) { // displays unordered values of my_ptrs
         cout << *my_ptrs[i] << " ";
     }
     
     cout << "\nSorted values of my_ptrs (least to greatest): ";
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4 - i - 1; j++) {
+    for (int i = 0; i < 4; i++) { // pushes least integers to leftmost side and greatest integers to rightmost side
+        for (int j = 0; j < 4 - i - 1; j++) { // 
             if (*my_ptrs[j] > *my_ptrs[j + 1]) {
-                int* temp = my_ptrs[j];
-                my_ptrs[j] = my_ptrs[j + 1];
-                my_ptrs[j + 1] = temp;
+                int* temp = my_ptrs[j]; // assign my_ptrs values to pointer temp variable
+                my_ptrs[j] = my_ptrs[j + 1]; // moves pointer value on right (j+1) to left side (j)
+                my_ptrs[j + 1] = temp; // assigns pointer temp value to "right" value in my_ptrs
             }
         }
     }
     
-    for (int i = 0; i < 4;i++) {
+    for (int i = 0; i < 4;i++) { // prints ordered version of my_ptrs
         std::cout << *my_ptrs[i] << " ";
     }
-    cout << "\nOld myInt array (should remain unchanged!)";
-    for (int i = 0; i < 4;i++) {
+    cout << "\nValues of myInt array (should remain unchanged!): ";
+    for (int i = 0; i < 4;i++) { // displays unchanged order of values in myInts
         std::cout << myInts[i] << " ";
     }
 }
