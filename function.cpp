fa94b109fcdf4f98d1cc4490b728eb16c72ea752
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2023-06-03 19:08:39 -0400

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,34 +1,32 @@
 // Paydreanne E. Hinton
 #include <iostream>
 
-// Function to swap the values of two integers
-void swap(int& num1, int& num2) {
-    int swap = num1; // swap will store num1
-    num1 = num2; // num1 will store num2
-    num2 = swap; // num2 will store num1's initial value
+// Swaps pointer parameter values
+void swap(int *num1, int *num2) {
+    int swap = *num1; // swap will store num1
+    *num1 = *num2; // num1 will store num2
+    *num2 = swap; // num2 will store num1's initial value
 }
 
 int main() {
+    // variables
     int num1, num2;
 
-    // Ask the user for input
     std::cout << "Initial value of num1: ";
-    std::cin >> num1;
+    std::cin >> num1; // takes num1 integer value
 
     std::cout << "Initial value of num2: ";
-    std::cin >> num2;
+    std::cin >> num2; // takes num2 integer value
 
-    // Call the swap function to swap the values
-    swap(num1, num2);
+    swap(&num1, &num2); // call memory address of num1 and num2
 
-    // Print the swapped values
-    std::cout << "\nSwapped value of num1: " << num1;
-    std::cout << "\nSwapped value of num2: " << num2;
+    std::cout << "\nSwapped value of num1: " << num1; // print new value of num1
+    std::cout << "\nSwapped value of num2: " << num2; // print new value of num2
     
     /*
     1) https://en.cppreference.com/w/cpp/algorithm/swap
     2) https://stackoverflow.com/questions/15672805/c-swapping-pointers
     */
-    
+
     return 0;
 }
