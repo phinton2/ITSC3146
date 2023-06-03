ea25da2057db208743ecdefa39dece33d164f1ed
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2023-06-03 19:02:51 -0400

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,3 +1,4 @@
+// Paydreanne E. Hinton
 #include <iostream>
 
 // Function to swap the values of two integers
@@ -23,6 +24,11 @@ int main() {
     // Print the swapped values
     std::cout << "\nSwapped value of num1: " << num1;
     std::cout << "\nSwapped value of num2: " << num2;
-
+    
+    /*
+    1) https://en.cppreference.com/w/cpp/algorithm/swap
+    2) https://stackoverflow.com/questions/15672805/c-swapping-pointers
+    */
+    
     return 0;
 }
